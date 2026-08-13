79af3cab98ef803182e8b8afb10597b3002731c8
knoobee98 <fdhuha17@gmail.com>
2026-08-13 15:55:44 +0700

    fix: use engines instead of invalid vercel functions runtime spec

--- api/[...slug].ts
+++ api/[...slug].ts
@@ -3,6 +3,10 @@ import { createApp } from '../src/server/app';
 
 const app = createApp();
 
+export const config = {
+  maxDuration: 30,
+};
+
 export default function handler(req: VercelRequest, res: VercelResponse) {
   app(req as any, res as any);
 }
